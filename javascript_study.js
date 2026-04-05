2b432c892f8a7db3d0307f457dafa42f51c46d87
SangVuong <vuonghoaisang@gmail.com>
2026-04-05 21:49:19 +0700

    thuchanh02

--- javascript_study.js
+++ javascript_study.js
@@ -1,66 +1,105 @@
-const saveBtn = document.querySelector(".btnSave");
-const inputTodo = document.querySelector(".name");
-if (saveBtn) {
-  saveBtn.addEventListener("click", function () {
-    const myTodo = {
-      id: Date.now(),
-      name: inputTodo.value,
-    };
-    const currentTodoStr = localStorage.getItem("todo");
-    if (currentTodoStr) {
-      const currentTodo = JSON.parse(currentTodoStr);
-      currentTodo.push(myTodo);
-      localStorage.setItem("todo", JSON.stringify(currentTodo));
-    } else {
-      localStorage.setItem("todo", JSON.stringify([myTodo]));
-    }
-    window.location.href = "index.html";
-  });
-}
-
-const generateTable = () => {
-  const todoListStr = localStorage.getItem("todo");
-  if (todoListStr) {
-    const todoList = JSON.parse(todoListStr);
-    console.log(todoList);
-
-    const tbody = document.querySelector("#todoList tbody");
-    if (todoList && todoList.length) {
-      todoList.forEach((todo, index) => {
-        tbody.innerHTML += `
+const fetchBlogs = async () => {
+  const res = await fetch("http://localhost:8000/blogs");
+  const data = await res.json();
+  const tbody = document.querySelector("#blogs tbody");
+  if (data && data.length) {
+    data.forEach((blog, index) => {
+      tbody.innerHTML += `
       <tr>
-        <td>${todo.id}</td>
-        <td>${todo.name}</td>
-        <td>
-          <button class="btnDelete" data-id="${todo.id}">Delete</button>
-        </td>
+        <td>${blog.id}</td>
+        <td>${blog.title}</td>
+        <td>${blog.author}</td>
+        <td>${blog.content}</td>
+        <td><button class="delete-btn" data-id="${blog.id}">Delete</button></td>
       </tr>`;
-      });
-    }
-  }
-};
-
-generateTable();
-
-const deleteBtns = document.querySelectorAll(".btnDelete");
-if (deleteBtns) {
-  deleteBtns.forEach((btn, index) => {
-    console.log(btn, index);
-    btn.addEventListener("click", function () {
-      const id = btn.getAttribute("data-id");
-      alert(id);
-      handleDeleteTodo(id);
     });
-  });
-}
-
-const handleDeleteTodo = (id) => {
-  const todoListStr = localStorage.getItem("todo");
-  if (todoListStr) {
-    const todoList = JSON.parse(todoListStr);
-    console.log(todoList, id);
-    const newTodo = todoList.filter((todo, index) => todo.id + "" !== id);
-    localStorage.setItem("todo", JSON.stringify(newTodo));
-    window.location.reload();
   }
 };
+
+const addNewRowToEnd = (blog) => {
+  const tableBody = document.querySelector("#blogs tbody");
+  const newRow = document.createElement("tr");
+  // Gán HTML cho dòng
+  newRow.innerHTML = `
+<tr>
+        <td>${blog.id}</td>
+        <td>${blog.title}</td>
+        <td>${blog.author}</td>
+        <td>${blog.content}</td>
+        <td><button class="delete-btn" data-id="${blog.id}">Delete</button></td>
+</tr>;
+`;
+  // Thêm dòng vào cuối bảng
+  tableBody.appendChild(newRow);
+  //fix bug
+  const btn = document.querySelector(`.delete-btn[data-id="${blog.id}"]`);
+  btn.addEventListener("click", async () => {
+    const id = btn.getAttribute("data-id");
+    //call api to delete a new blog
+    const rawResponse = await fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await rawResponse.json();
+    //delete html row
+    const row = btn.closest("tr");
+    row.remove();
+  });
+};
+
+const handleAddNewBlog = () => {
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const content = document.getElementById("content");
+  const saveBlogBtn = document.getElementById("saveBlog");
+  saveBlogBtn.addEventListener("click", async () => {
+    console.log(title.value, author.value, content.value);
+
+    //call api to create a new blog
+    const rawResponse = await fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title.value,
+        author: author.value,
+        content: content.value,
+      }),
+    });
+    const data = await rawResponse.json();
+    addNewRowToEnd(data);
+    console.log("Phan hoi api create: ", data);
+  });
+};
+
+const handleDeleteBlog = () => {
+  const btns = document.querySelectorAll(".delete-btn");
+  if (btns) {
+    btns.forEach((btn, index) => {
+      btn.addEventListener("click", async () => {
+        const id = btn.getAttribute("data-id");
+        //call api to delete a new blog
+        const rawResponse = await fetch(`http://localhost:8000/blogs/${id}`, {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await rawResponse.json();
+        //delete html row
+        const row = btn.closest("tr");
+        row.remove();
+      });
+    });
+  }
+};
+fetchBlogs().then(() => {
+  handleDeleteBlog();
+});
+handleAddNewBlog();
